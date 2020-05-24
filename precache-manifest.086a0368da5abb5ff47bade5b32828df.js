26e1077e863079f9de185e9be0d625c87335932b
Srikrishna <krishna.srikrishna24@gmail.com>
2020-05-24 17:23:08 -0400

    Updates

---
+++ precache-manifest.086a0368da5abb5ff47bade5b32828df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612cd75eae0ca8ce971666f9c1e3e3c3",
+    "url": "/jsonplaceholder-sample/index.html"
+  },
+  {
+    "revision": "d2d0d73d713556fb2727",
+    "url": "/jsonplaceholder-sample/static/js/2.cf151472.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/jsonplaceholder-sample/static/js/2.cf151472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2090be5a740ddaa16a26",
+    "url": "/jsonplaceholder-sample/static/js/main.5039b2c7.chunk.js"
+  },
+  {
+    "revision": "5885b92a257e4fca4138",
+    "url": "/jsonplaceholder-sample/static/js/runtime-main.519599c2.js"
+  }
+]);
